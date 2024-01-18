4b00483169dc14458faa40b761187d6bd6475184
REDzHUB <138633493+REDzHUB@users.noreply.github.com>
2024-01-18 20:26:53 -0300

    Update and rename Teste to Source.lua

---
+++ Source.lua
@@ -0,0 +1,16 @@
+local request = http_request or request or HttpPost or syn.request
+local HttpService = game:GetService("HttpService")
+local Webhook = {}
+
+function Webhook.new(Token, Body)
+	request({
+    Url = Token,
+    Method = "POST",
+    Headers = {
+      ["Content-Type"] = "application/json",
+    },
+    Body = HttpService:JSONEncode(Body),
+  })
+end
+
+return Webhook
